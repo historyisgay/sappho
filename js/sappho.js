16227b0b35817d8a680ece5f8be5f3a9e0e871f3
Laura Galm <lauraugalm@gmail.com>
2020-03-10 21:24:48 -0400

    ended up removing the fade in text wow I cant focus

--- js/sappho.js
+++ js/sappho.js
@@ -33,7 +33,7 @@ function generate(poem, fragments, number)
 		remove(inline,index);
 	}
 	//fragmentTag.innerHTML = fragmentsHtml;
-	inlineTag.innerHTML = message;
+	inlineTag.innerHTML = inlineProse;
 }
 
 function remove(arr,index)
